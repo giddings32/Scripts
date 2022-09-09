f730067a02e1f2393c114ba7ad274e584e3f7c94
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-09-08 22:58:05 -0400

    Update enumerateAD.ps1

--- Active_Directory/enumerateAD.ps1
+++ Active_Directory/enumerateAD.ps1
@@ -1,28 +1,15 @@
-# Edit lines 21-24 and uncomment filter you will be using
-
 $domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-
 $PDC = ($domainObj.PdcRoleOwner).Name
-
 $SearchString = "LDAP://"
-
 $SearchString += $PDC + "/"
-
 $DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
-
 $SearchString += $DistinguishedName
-
 $Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
-
 $objDomain = New-Object System.DirectoryServices.DirectoryEntry
-
 $Searcher.SearchRoot = $objDomain
-
-$group = "Domain Admins"
 #$Searcher.filter="samAccountType=805306368"
 $Searcher.filter="name=Jeff_Admin"
 #$Searcher.filter="(&(objectClass=Group)(name=" + $group + "))"
-
 $Searcher.FindAll()
 Foreach($obj in $Result)
 {

